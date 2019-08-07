58fad97729e3e20d8a0ef0344297eceff7ac9e51
iyangar <43437020+iyangar@users.noreply.github.com>
2019-08-07 16:52:37 +0100

    Create PhenotypeServiceApplication.java

---
+++ ebi/swagger/testampt2dservice/rest/PhenotypeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ebi.metadata.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhenotypeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhenotypeServiceApplication.class, args);
+    }
+
+}
