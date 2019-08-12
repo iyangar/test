d574810ba663e31e04249314928e7ca3f34d8e43
iyangar <43437020+iyangar@users.noreply.github.com>
2019-08-12 10:05:38 +0100

    Update PhenotypesRestController.java

--- ebi/swagger/testampt2dservice/src/main/java/org/ebi/metadata/rest/PhenotypesRestController.java
+++ ebi/swagger/testampt2dservice/src/main/java/org/ebi/metadata/rest/PhenotypesRestController.java
@@ -2,13 +2,11 @@ package org.ebi.metadata.rest;
 
 import org.ebi.metadata.api.PhenotypesApi;
 import org.ebi.metadata.api.model.Phenotype;
-import org.ebi.metadata.api.model.PhenotypeWithHref;
-import org.ebi.metadata.api.model.PhenotypeWithHrefLinks;
 import org.ebi.metadata.api.model.PhenotypeWithoutId;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 public class PhenotypesRestController implements PhenotypesApi {
@@ -16,28 +14,32 @@ public class PhenotypesRestController implements PhenotypesApi {
     public ResponseEntity<Phenotype> getPhenotypeEntity(String page, String size, String sort) {
         Phenotype phenotype = new Phenotype();
         phenotype.setType(Phenotype.TypeEnum.DICHOTOMOUS);
-        phenotype.setId("1");
+        phenotype.setPhid("1");
         phenotype.setPhenotypeGroup(Phenotype.PhenotypeGroupEnum.ANTHROPOMETRIC);
         phenotype.setDescription("Evaluation phenotype");
         phenotype.setAllowedValues("All allowed values");
+
+        Link link = ControllerLinkBuilder
+                .linkTo(PhenotypesRestController.class)
+                .slash(phenotype.getPhid())
+                .withSelfRel();
+        phenotype.add(link);
         return ResponseEntity.ok(phenotype);
     }
 
-    public ResponseEntity<PhenotypeWithHref> postPhenotypeEntity(PhenotypeWithoutId phenotypeWithoutId) {
-        PhenotypeWithHref phenotypeWithHref = new PhenotypeWithHref();
-        phenotypeWithHref.setType(PhenotypeWithHref.TypeEnum.values()[phenotypeWithoutId.getType().ordinal()]);
-        phenotypeWithHref.setId("2");
-        phenotypeWithHref.setPhenotypeGroup(PhenotypeWithHref.PhenotypeGroupEnum.values()[phenotypeWithoutId.getPhenotypeGroup().ordinal()]);
-        phenotypeWithHref.setDescription(phenotypeWithoutId.getDescription());
-        phenotypeWithHref.setAllowedValues(phenotypeWithoutId.getAllowedValues());
+    public ResponseEntity<Phenotype> postPhenotypeEntity(PhenotypeWithoutId phenotypeWithoutId) {
+        Phenotype phenotype = new Phenotype();
+        phenotype.setType(Phenotype.TypeEnum.values()[phenotypeWithoutId.getType().ordinal()]);
+        phenotype.setPhid("3");
+        phenotype.setPhenotypeGroup(Phenotype.PhenotypeGroupEnum.values()[phenotypeWithoutId.getPhenotypeGroup().ordinal()]);
+        phenotype.setDescription(phenotypeWithoutId.getDescription());
+        phenotype.setAllowedValues(phenotypeWithoutId.getAllowedValues());
 
-        ArrayList<PhenotypeWithHrefLinks> phenotypeWithHrefLinksList = new ArrayList<>();
-        PhenotypeWithHrefLinks phenotypeWithHrefLinks = new PhenotypeWithHrefLinks();
-        phenotypeWithHrefLinks.setHref("http://localhost:8080/phenotypes/1");
-        phenotypeWithHrefLinks.setTemplated(true);
-        phenotypeWithHrefLinks.setRel("Release");
-        phenotypeWithHrefLinksList.add(phenotypeWithHrefLinks);
-        phenotypeWithHref.setLinks(phenotypeWithHrefLinksList);
-        return ResponseEntity.ok(phenotypeWithHref);
+        Link link = ControllerLinkBuilder
+                .linkTo(PhenotypesRestController.class)
+                .slash(phenotype.getPhid())
+                .withSelfRel();
+        phenotype.add(link);
+        return ResponseEntity.ok(phenotype);
     }
 }
